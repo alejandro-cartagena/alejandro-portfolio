817a23197651f465cc05d17c409b0c4739fd3b4f
Alejandro <alejandrocartagena68@gmail.com>
2024-07-16 15:10:48 -0400

    Updated Google Analytics to work with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import ReactGA from "react-ga4";
 import useDrift from "./useDrift";
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 
 function App() {
   useDrift("kms2k4twfvaz");
+
+  useEffect(() => {
+    ReactGA.initialize("G-K8G4N5FWZZ"); // Replace with your GA4 tracking ID
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
   return (
     <div className="lighter-black flex flex-col min-h-[100vh]">
       <Navbar />
