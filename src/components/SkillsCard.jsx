e2b25e23deea604c32bf573a431af4ce86163d13
Alejandro <alejandrocartagena68@gmail.com>
2024-08-27 19:24:50 -0400

    Hover effect on skills cards added, removed opacity hover effect on portfolio card

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SkillsCard({ icon, text }) {
   return (
-    <div className="flex items-center shadow-md p-6">
+    <div className="flex items-center shadow-md p-6 ease-in-out duration-200 hover:scale-105">
       <img className="mr-4" src={icon} alt={`${text} Icon`} />
       <p className="font-bold text-2xl">{text}</p>
     </div>
