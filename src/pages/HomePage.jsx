b132b114b4570c7e240f6c914c4a75fc4717b5cd
Alejandro <alejandrocartagena68@gmail.com>
2024-08-27 20:24:30 -0400

    Added animation on scroll

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,14 +5,49 @@ import AboutSection from "../components/AboutSection";
 import PortfolioSection from "../components/PortfolioSection";
 import ContactSection from "../components/ContactSection";
 
+// Animate on Scroll Import
+import "animate.css/animate.compat.css";
+import ScrollAnimation from "react-animate-on-scroll";
+
 function HomePage() {
   return (
     <>
       <HeroSection />
-      <SkillsSection />
-      <AboutSection />
-      <PortfolioSection />
-      <ContactSection />
+      <ScrollAnimation
+        animateIn="fadeIn"
+        duration={0.75}
+        offset={360}
+        animateOnce={true}
+      >
+        <SkillsSection />
+      </ScrollAnimation>
+
+      <ScrollAnimation
+        animateIn="fadeIn"
+        duration={0.75}
+        offset={360}
+        animateOnce={true}
+      >
+        <AboutSection />
+      </ScrollAnimation>
+
+      <ScrollAnimation
+        animateIn="fadeIn"
+        duration={0.75}
+        offset={360}
+        animateOnce={true}
+      >
+        <PortfolioSection />
+      </ScrollAnimation>
+
+      <ScrollAnimation
+        animateIn="fadeIn"
+        duration={0.75}
+        offset={300}
+        animateOnce={true}
+      >
+        <ContactSection />
+      </ScrollAnimation>
     </>
   );
 }
